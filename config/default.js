b2051feb14f48930c13519a7c467a4875b42d788
minoli vartak <minoli879@gmail.com>
2019-08-02 10:56:24 +0530

    first commit

---
+++ config/default.js
@@ -0,0 +1,9 @@
+module.exports = exports = {
+    port: 3000,
+    // Contentstack Config
+    contentstack: {
+        api_key: 'bltfa9026798819a8a2',
+        access_token: 'blt726d97da73b0c30b',
+       // environment: 'development'
+    }
+};
